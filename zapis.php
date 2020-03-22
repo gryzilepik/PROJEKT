f7eff6b2b7e343a278bbbdc509f62e75b454ec83
gryzilepik <62503060+gryzilepik@users.noreply.github.com>
2020-03-22 13:58:16 +0100

    Update zapis.php

--- zapis.php
+++ zapis.php
@@ -1,4 +1,9 @@
 <?php
     $pol = mysqli_connect('localhost', 'root', '', 'projekt');
-    
+    $gracz = $_GET['nazwa_gracza'];
+    $wynik = $_GET['wynik'];
+    $zap = "INSERT INTO tabela VALUES('$gracz', '$wynik')";
+    if($gracz != "" || $gracz != NULL)
+        $wynik = mysqli_query($pol, $zap);
+    //SELECT nazwa_gracza, wynik FROM tabela ORDER BY wynik DESC LIMIT 10
 ?>
