2ccb98dc09d62bf65a2effcd78eaf68eccb4ecac
gryzilepik <62503060+gryzilepik@users.noreply.github.com>
2020-04-09 11:46:03 +0200

    Add files via upload

--- wojny.js
+++ wojny.js
@@ -12,7 +12,9 @@ function nowaGra()
     plansza.id = "plansza";
     main.appendChild(plansza);   
     p1 = document.createElement('div');
+    p1.id = "p1";
     plansza.appendChild(p1);
     p2 = document.createElement('div');
+    p2.id = "p2";
     plansza.appendChild(p2);
 }
